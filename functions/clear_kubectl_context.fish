1c3393897b053dfc1bdabc323c49e9e004776c7d
Tom Lazar <tomster@pyfidelity.com>
2018-11-20 14:29:42 +0100

    Adjust variable names

--- functions/clear_kubectl_context.fish
+++ functions/clear_kubectl_context.fish
@@ -1,5 +1,7 @@
+# a helper function to clear the context caches
+# only needed during development or when the definition of a context has changed
 function clear_kubectl_context
-    rm $context_cache 
-    rm $cluster_cache 
-    rm $namespace_cache 
+    rm $kubectl_context_cache 
+    rm $kubectl_cluster_cache 
+    rm $kubectl_namespace_cache 
 end
